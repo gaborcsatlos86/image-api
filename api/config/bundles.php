7befe658fb123c0b9dfcbf9c2fda13721c53f358
Csatlos Gabor <gabor.csatlos86@gmail.com>
2024-11-19 19:10:42 +0100

    feat(init)[P-IMG-2]: config modifications

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -13,4 +13,7 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
+    Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
